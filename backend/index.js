1e051ef8cf75262dcbd95b834614bda44b4b33ec
yousef mohamed <joetiger22222@gmail.com>
2023-09-19 12:04:42 +0300

    CRUD Operation BackEnd

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors');
+const clothesRoute = require('./routes/ClothesRoute')
+const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/ItiClothes')
+app.use(express.json());
+app.use(clothesRoute)
+
+
+
+
+
+
+app.listen(3001,()=>{
+    console.log('listening')
+})
